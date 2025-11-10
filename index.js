ba712fcf70d99476eb6144d063b8eaf4db3fa5f3
Edgar <edgar.muoni@gmail.com>
2025-11-10 14:20:31 +0200

    Update

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); //cross origin resource error avoid
+app.use(express.json());
+
+const thingamabobs = [
+    {id: 1, name: "plumbus", price: 34.59}
+    {id: 2, name: "van furby", price: 666}
+    {id: 3, name: "sapakas", price: 2000}
+];
+
+app.get('/thingamabobs', (req, res) => {res.send(thingamabobs)})
+
+app.get('/thingamabobs/:id', (req, res) => 
+{
+    if (typeof thingamabobs[req.params.id - 1] === 'undefined') 
+    {
+        return res.status(404).send({error:"Object not found. Check your thingamabob id."});
+    }
+    
+})
+
+app.post('/thingamabobs', (req, res) => {
+    if (!req.body.name || !req.body.price) 
+    {
+        return res.status(400).send({error:"One or multiple parameters are missing"});  
+    }
+    let newThingy = {
+        id: thingamabobs.length + 1,
+        name: req.body.name,
+        price: req.body.price
+    }
+
+    thingamabobs.push(newThingy);
+    res.send(newThingy);
+    res.status(201).location('localhost:8080/thingamabobs/' + (thingamabobs.length - 1)).
+    send(newThingy);
+})
+
+app.delete('/thingamabobs/:id', (req, res) => 
+{
+    if (!req.body.name || !req.body.price) 
+    {
+        return res.status(400).send({error:"One or multiple parameters are missing"});  
+    }
+    thingamabobs.splice(req.params.id - 1, 1);
+    res.status(204).send();
+})
+
+app.listen(8080, () => {console.log(`API running at: http://localhost:8080`)})
